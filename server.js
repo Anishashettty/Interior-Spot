ab714e730abff972db4c1bc4d5a93c4409205fa8
Anishashettty <shettyanisha2004@gmail.com>
2025-02-24 13:34:45 +0530

    add

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+
+// Serve static files (CSS, JS, images)
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home Page", message: "Welcome to My Website!" });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About Page" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
